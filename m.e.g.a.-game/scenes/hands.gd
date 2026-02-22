3cfb6629b00ac4f6f71f36c07b9b34c59998e5df
ghebert3 <geraldhebert3@gmail.com>
2026-02-22 10:31:45 -0600

    right click to unclamp hand

--- m.e.g.a.-game/scenes/hands.gd
+++ m.e.g.a.-game/scenes/hands.gd
@@ -5,19 +5,38 @@ extends Sprite2D
 @export var min_y := 700.0
 @export var max_y := 1650.0
 
+var free_mode := false  
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
 
 func _process(delta):
+
+	# Toggle on right click
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT) and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		pass
+
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		pass
+
+	if Input.is_action_just_pressed("ui_right_click"):
+		free_mode = !free_mode
+
 	var mouse := get_global_mouse_position()
-	
-	var clamped_x = clamp(mouse.x,400, 1500)
-	var clamped_y = clamp(mouse.y, 700, 1650)
 
-	var clamped_pos = Vector2(clamped_x, clamped_y)
+	if free_mode:
+		# FREE MODE
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		global_position = mouse
+	else:
+		# CLAMP MODE
+		Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
 
-	# Move hand
-	global_position = clamped_pos
+		var clamped_x = clamp(mouse.x, min_x, max_x)
+		var clamped_y = clamp(mouse.y, min_y, max_y)
+		var clamped_pos = Vector2(clamped_x, clamped_y)
 
-	if mouse.distance_to(clamped_pos) > 0.5:
-		get_viewport().warp_mouse(clamped_pos)
+		global_position = clamped_pos
+
+		if mouse.distance_to(clamped_pos) > 0.5:
+			get_viewport().warp_mouse(clamped_pos)
