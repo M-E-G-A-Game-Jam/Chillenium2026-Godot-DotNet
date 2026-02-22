a30bc61342d4f3bae7703fdb6b19e9fbe9c2dc1d
ghebert3 <geraldhebert3@gmail.com>
2026-02-22 02:12:41 -0600

    Added Stuff

--- m.e.g.a.-game/scenes/hands.gd
+++ m.e.g.a.-game/scenes/hands.gd
@@ -5,9 +5,12 @@ extends Sprite2D
 @export var min_y := 700.0
 @export var max_y := 1650.0
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+
 func _process(delta):
 	var mouse := get_global_mouse_position()
-
+	
 	var clamped_x = clamp(mouse.x,400, 1500)
 	var clamped_y = clamp(mouse.y, 700, 1650)
 
