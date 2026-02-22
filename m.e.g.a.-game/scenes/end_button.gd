f594947f6d29f9238c259913a3bcb3a3f8770b22
ghebert3 <geraldhebert3@gmail.com>
2026-02-22 12:37:45 -0600

    Revert "Merge branch 'EllaCombinedGerhald3' into gerald-branch-3rd"
    This reverts commit d73199d9945d83750d0bf1399b8624b24b8bd340, reversing
    changes made to d1777c475dacfc66b92be4a11c96674d2c2a9e6e.

--- m.e.g.a.-game/scenes/end_button.gd
+++ m.e.g.a.-game/scenes/end_button.gd
@@ -6,6 +6,7 @@ extends TextureButton
 @onready var exit_guy_group = $"../../ExitGuyGroup"
 @onready var start_button = $"../startButton"
 @onready var end_button = $"../endButton"
+@onready var hoverend_sfx: AudioStreamPlayer = $hoverend_sfx
 
 @onready var start_sprite: AnimatedSprite2D = start_button.get_node("AnimatedSprite2D")
 @onready var end_sprite: AnimatedSprite2D  = end_button.get_node("AnimatedSprite2D")
@@ -26,7 +27,7 @@ func _on_mouse_entered() -> void:
 	noramal_cloud_group.visible = false
 	start_guy_group.visible = false
 	exit_guy_group.visible = true
-	
+	hoverend_sfx.play()
 	end_sprite.pause()
 
 func _on_mouse_exited() -> void:
