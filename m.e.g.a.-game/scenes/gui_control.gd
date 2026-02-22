07f3d43612d1ec61d3057debfc85cb70d86cd330
iwasella <145622813+iwasella@users.noreply.github.com>
2026-02-22 11:06:51 -0600

    Fixed the buttons & fixed the text layout

--- m.e.g.a.-game/scenes/gui_control.gd
+++ m.e.g.a.-game/scenes/gui_control.gd
@@ -3,102 +3,101 @@ extends Control
 #initiralizing a Random Generator
 var rng = RandomNumberGenerator.new()
 
-
 var question_data = {
 	"0-5": [
-		{"text": "Learn to read?", "yes": 1, "no": -1},
-		{"text": "Eat a penny?", "yes": -1, "no": 1},
-		{"text": "Play with a big tower of legos, twin?", "yes": 1, "no": -1},
-		{"text": "Slide down the very steep slide, twin?", "yes": -1, "no": 1},
-		{"text": "Wait until timeout is over, twin?", "yes": 1, "no": -1},
-		{"text": "Run across a creek filled with slippery rocks, twin?", "yes": -1, "no": 1},
-		{"text": "Yell at Mike the Tiger through a broken fence?", "yes": -1, "no": 1},
-		{"text": "Sneak into a closed ride at ThisKnee World?", "yes": -1, "no": 1},
-		{"text": "Touch a hot stove because it looks enticing?", "yes": -5, "no": 1}
+		{"text": "Learn to read?\n", "yes": 1, "no": -1},
+		{"text": "Eat a penny?\n", "yes": -1, "no": 1}, # Negative to eat pennies!
+		{"text": "Play with a big tower of legos, twin?\n", "yes": 1, "no": -1},
+		{"text": "Slide down the very steep slide, twin?\n", "yes": 1, "no": -1},
+		{"text": "Wait until timeout is over, twin?\n", "yes": 1, "no": -1},
+		{"text": "Run across a creek filled with slippery rocks, twin?\n", "yes": 1, "no": -1},
+		{"text": "Yell at Mike the Tiger through a broken fence?\n", "yes": 1, "no": -1},
+		{"text": "Sneak into a closed ride at ThisKnee World?\n", "yes": 1, "no": -1},
+		{"text": "Touch a hot stove because it looks enticing?\n", "yes": -5, "no": 1}
 	],
 	"6-10": [
-		{"text": "Bully a classmate?", "yes": -1, "no": 1},
-		{"text": "Join a sports team?", "yes": 1, "no": -1},
-		{"text": "Slide down a slide that is way too tall for you?", "yes": -1, "no": 1},
-		{"text": "Run a lemonade stand?", "yes": 1, "no": -1},
-		{"text": "Grow taller?", "yes": 1, "no": 0},
-		{"text": "Eat way too much birthday cake?", "yes": -1, "no": 1},
-		{"text": "Sign up for Soccer?", "yes": 1, "no": -1},
-		{"text": "Sign up for Basketball?", "yes": 1, "no": -1},
-		{"text": "Sign up for Baseball?", "yes": 1, "no": -1},
-		{"text": "Sign up for American Football?", "yes": -1, "no": 1}, # High injury risk!
-		{"text": "Sign up for Gymnastics?", "yes": 1, "no": -1}
+		{"text": "Bully a classmate?\n", "yes": -1, "no": 1},
+		{"text": "Join a sports team?\n", "yes": 1, "no": -1},
+		{"text": "Slide down a slide that is way too tall for you?\n", "yes": 1, "no": -1},
+		{"text": "Run a lemonade stand?\n", "yes": 1, "no": -1},
+		{"text": "Grow taller?\n", "yes": 1, "no": 0},
+		{"text": "Eat way too much birthday cake?\n", "yes": -1, "no": 1},
+		{"text": "Sign up for Soccer?\n", "yes": 1, "no": -1},
+		{"text": "Sign up for Basketball?\n", "yes": 1, "no": -1},
+		{"text": "Sign up for Baseball?\n", "yes": 1, "no": -1},
+		{"text": "Sign up for American Football?\n", "yes": 1, "no": -1},
+		{"text": "Sign up for Gymnastics?\n", "yes": 1, "no": -1}
 	],
 	"11-20": [
-		{"text": "Start smoking?", "yes": -1, "no": 1},
-		{"text": "Join the school choir?", "yes": 1, "no": -1},
-		{"text": "Sign up for Rugby?", "yes": -1, "no": 1},
-		{"text": "Sign up for Private School?", "yes": 1, "no": -1},
-		{"text": "Climb 'Beeg Roc' while rock climbing?", "yes": -1, "no": 1},
-		{"text": "Go inside the Haunted House?", "yes": -1, "no": 1},
-		{"text": "Join the American Parkour Samurai Gladiator Challenge?", "yes": -1, "no": 1},
-		{"text": "Have a big growth spurt?", "yes": 1, "no": 0},
-		{"text": "Spar with your twin just for fun?", "yes": -1, "no": 1},
-		{"text": "Go to a dangerous Black Friday Sale at BallMart?", "yes": -1, "no": 1},
-		{"text": "Try to solve a coding bug as fast as possible?", "yes": -1, "no": 1},
-		{"text": "Keep the 'Life Note' and write your name in it?", "yes": -10, "no": 1},
-		{"text": "Play 'Umamusume: John Darby' gacha game?", "yes": -1, "no": 1},
-		{"text": "Study early for the big test?", "yes": 1, "no": -1},
-		{"text": "Spam dank memes everywhere you go?", "yes": -1, "no": 1}
+		{"text": "Start smoking?\n", "yes": -1, "no": 1},
+		{"text": "Join the school choir?\n", "yes": 1, "no": -1},
+		{"text": "Sign up for Rugby?\n", "yes": 1, "no": -1},
+		{"text": "Sign up for Private School?\n", "yes": 1, "no": -1},
+		{"text": "Climb 'Beeg Roc' while rock climbing?\n", "yes": 1, "no": -1},
+		{"text": "Go inside the Haunted House?\n", "yes": 1, "no": -1},
+		{"text": "Join the American Parkour Samurai Gladiator Challenge?\n", "yes": 1, "no": -1},
+		{"text": "Have a big growth spurt?\n", "yes": 1, "no": 0},
+		{"text": "Spar with your twin just for fun?\n", "yes": 1, "no": -1},
+		{"text": "Go to a dangerous Black Friday Sale at BallMart?\n", "yes": 1, "no": -1},
+		{"text": "Try to solve a coding bug as fast as possible?\n", "yes": 1, "no": -1},
+		{"text": "Keep the 'Life Note' and write your name in it?\n", "yes": -10, "no": 1},
+		{"text": "Play 'Umamusume: John Darby' gacha game?\n", "yes": 1, "no": -1},
+		{"text": "Study early for the big test?\n", "yes": 1, "no": -1},
+		{"text": "Spam dank memes everywhere you go?\n", "yes": 1, "no": -1}
 	],
 	"21-30": [
-		{"text": "Go to a frat party?", "yes": -1, "no": 1},
-		{"text": "Climb 'Beeg Roc'?", "yes": -1, "no": 1},
-		{"text": "Keep the 'Life Note' and write your name in it?", "yes": -10, "no": 1},
-		{"text": "Your twin asks if you'd lose a certain fight. Would you lose?", "yes": -1, "no": 1}
+		{"text": "Go to a frat party?\n", "yes": 1, "no": -1},
+		{"text": "Climb 'Beeg Roc'?\n", "yes": 1, "no": -1},
+		{"text": "Keep the 'Life Note' and write your name in it?\n", "yes": -10, "no": 1},
+		{"text": "Your twin asks if you'd lose a certain fight. Would you lose?\n", "yes": -1, "no": 1}
 	],
 	"31-40": [
-		{"text": "Have children?", "yes": -1, "no": 1}, # Kids are stressful!
-		{"text": "Talk to the skrunkly guy at the Gardening Addiction support group?", "yes": 1, "no": -1},
-		{"text": "Cheat on your significant other?", "yes": -2, "no": 1},
-		{"text": "A stray dog bit you. Go to the doctor even if insurance might not cover it?", "yes": 1, "no": -2},
-		{"text": "Invest everything into Bitcoin?", "yes": -1, "no": 1},
-		{"text": "Bail your hairdresser out of jail for the Nair incident?", "yes": -1, "no": 1},
-		{"text": "Go bald voluntarily?", "yes": 1, "no": 0},
-		{"text": "Quit your job to follow your dreams?", "yes": 1, "no": -1},
-		{"text": "Max out your 401k contribution?", "yes": 1, "no": -1},
-		{"text": "Max out your credit card on a jet ski?", "yes": -1, "no": 1}
+		{"text": "Have children?\n", "yes": 1, "no": -1},
+		{"text": "Talk to the skrunkly guy at the Gardening Addiction support group?\n", "yes": 1, "no": -1},
+		{"text": "Cheat on your significant other?\n", "yes": -2, "no": 1},
+		{"text": "A stray dog bit you. Go to the doctor even if insurance might not cover it?\n", "yes": 1, "no": -2},
+		{"text": "Invest everything into Bitcoin?\n", "yes": 1, "no": -1},
+		{"text": "Bail your hairdresser out of jail for the Nair incident?\n", "yes": 1, "no": -1},
+		{"text": "Go bald voluntarily?\n", "yes": 1, "no": 0},
+		{"text": "Quit your job to follow your dreams?\n", "yes": 1, "no": -1},
+		{"text": "Max out your 401k contribution?\n", "yes": 1, "no": -1},
+		{"text": "Max out your credit card on a jet ski?\n", "yes": 1, "no": -1}
 	],
 	"41-50": [
-		{"text": "Buy a mid-life crisis sports car?", "yes": -1, "no": 1},
-		{"text": "Start doing yoga to fix your 'bad back'?", "yes": 1, "no": -1},
-		{"text": "Try to DIY fix the electrical wiring in your house?", "yes": -2, "no": 1},
-		{"text": "Yell at the neighbor's kid for stepping on your lawn?", "yes": -1, "no": 1},
-		{"text": "Go on a strictly 'Juice Cleanse' diet for a month?", "yes": -1, "no": 1}
+		{"text": "Buy a mid-life crisis sports car?\n", "yes": 1, "no": -1},
+		{"text": "Start doing yoga to fix your 'bad back'?\n", "yes": 1, "no": -1},
+		{"text": "Try to DIY fix the electrical wiring in your house?\n", "yes": -2, "no": 1},
+		{"text": "Yell at the neighbor's kid for stepping on your lawn?\n", "yes": 1, "no": -1},
+		{"text": "Go on a strictly 'Juice Cleanse' diet for a month?\n", "yes": 1, "no": -1}
 	],
 	"51-60": [
-		{"text": "Retire early and live on a boat?", "yes": 1, "no": -1},
-		{"text": "Take up extreme bird watching?", "yes": 1, "no": 0},
-		{"text": "Start a fight with a goose at the local pond?", "yes": -1, "no": 1},
-		{"text": "Learn how to use 'The TikTok'?", "yes": -1, "no": 1},
-		{"text": "Eat a steak every single night?", "yes": -1, "no": 1}
+		{"text": "Retire early and live on a boat?\n", "yes": 1, "no": -1},
+		{"text": "Take up extreme bird watching?\n", "yes": 1, "no": 0},
+		{"text": "Start a fight with a goose at the local pond?\n", "yes": 1, "no": -1},
+		{"text": "Learn how to use 'The TikTok'?\n", "yes": 1, "no": -1},
+		{"text": "Eat a steak every single night?\n", "yes": 1, "no": -1}
 	],
 	"61-70": [
-		{"text": "Move into a retirement community with a great pool?", "yes": 1, "no": -1},
-		{"text": "Refuse to wear your hearing aid?", "yes": -1, "no": 1},
-		{"text": "Drive your car through the front of a pharmacy accidentally?", "yes": -2, "no": 1},
-		{"text": "Take up competitive Bingo?", "yes": 1, "no": 0}
+		{"text": "Move into a retirement community with a great pool?\n", "yes": 1, "no": -1},
+		{"text": "Refuse to wear your hearing aid?\n", "yes": -1, "no": 1},
+		{"text": "Drive your car through the front of a pharmacy accidentally?\n", "yes": -2, "no": 1},
+		{"text": "Take up competitive Bingo?\n", "yes": 1, "no": 0}
 	],
 	"71-80": [
-		{"text": "Try to do a backflip to impress your grandkids?", "yes": -5, "no": 1},
-		{"text": "Eat nothing but soft prunes?", "yes": 1, "no": -1},
-		{"text": "Argue with a ghost in the hallway?", "yes": -1, "no": 1},
-		{"text": "Finally tell your twin what you really think of them?", "yes": 1, "no": -1}
+		{"text": "Try to do a backflip to impress your grandkids?\n", "yes": -5, "no": 1},
+		{"text": "Eat nothing but soft prunes?\n", "yes": 1, "no": -1},
+		{"text": "Argue with a ghost in the hallway?\n", "yes": -1, "no": 1},
+		{"text": "Finally tell your twin what you really think of them?\n", "yes": 1, "no": -1}
 	],
 	"81-90": [
-		{"text": "Attempt to outrun a snail?", "yes": -1, "no": 1},
-		{"text": "Take a 4-hour nap in the middle of the grocery store?", "yes": 1, "no": -1},
-		{"text": "Mistake the TV remote for a telephone?", "yes": 0, "no": 0}
+		{"text": "Attempt to outrun a snail?\n", "yes": 1, "no": -1},
+		{"text": "Take a 4-hour nap in the middle of the grocery store?\n", "yes": 1, "no": -1},
+		{"text": "Mistake the TV remote for a telephone?\n", "yes": 0, "no": 0}
 	],
 	"91-100": [
-		{"text": "Breathe deeply?", "yes": 1, "no": -10},
-		{"text": "Try to remember where you put your teeth?", "yes": 1, "no": -1},
-		{"text": "Ascend to a higher plane of existence?", "yes": -10, "no": 1}
+		{"text": "Breathe deeply?\n", "yes": 1, "no": -10},
+		{"text": "Try to remember where you put your teeth?\n", "yes": 1, "no": -1},
+		{"text": "Ascend to a higher plane of existence?\n", "yes": 1, "no": -10}
 	]
 }
 
